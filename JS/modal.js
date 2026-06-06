0ec1492da84d7675b48ebb58dd134acb0dd725ca
yUMIP04 <yumip5719@gmail.com>
2026-06-05 22:13:08 -0600

    Dandole esilo al modal y acomodando sus componenetes

---
+++ JS/modal.js
@@ -0,0 +1,29 @@
+console.log("El JS del modal si funciona");
+
+const Proyecto1 = document.querySelector(".one");
+const Modal = document.querySelector(".modal");
+const btn_Cerrar = document.querySelector(".btn-cerrar");
+
+function Open_Modal(btn_proyect, modal, btn_cerrar){
+
+    if(btn_proyect && modal && btn_cerrar){
+
+    
+    btn_proyect.addEventListener("click", (e) =>{
+
+        modal.classList.add("active");
+
+        btn_cerrar.addEventListener("click", (e) =>{
+            
+            modal.classList.remove("active");
+    })
+
+    })
+
+    } else{
+        console.log("Hubo un error");
+    }
+}
+
+Open_Modal(Proyecto1, Modal, btn_Cerrar);
+
