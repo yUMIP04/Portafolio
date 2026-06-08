0166ad7a8b536434f2f07b27f4e0e8c2143acfc7
yUMIP04 <yumip5719@gmail.com>
2026-06-07 22:00:27 -0600

    primero modal listo, -adjuntando mas imagenes para el segundo modal

--- JS/modal.js
+++ JS/modal.js
@@ -1,6 +1,8 @@
 console.log("El JS del modal si funciona");
 
 const Proyecto1 = document.querySelector(".one");
+const Proyecto2 = document.querySelector(".two");
+
 const Modal = document.querySelector(".modal");
 const btn_Cerrar = document.querySelector(".btn-cerrar");
 
@@ -26,8 +28,55 @@ function Open_Modal(btn_proyect, modal, btn_cerrar){
 }
 
 Open_Modal(Proyecto1, Modal, btn_Cerrar);
-
+Open_Modal(Proyecto2, Modal, btn_Cerrar );
 
 /*Imagenes en modal */
 
-const ImagenesData = ["/assets/img/inicioSesion_MVLS.png", "/assets/img/MVLS-videollamada.png"];
+const ImagenesDataMVLS = ["/assets/img/inicioSesion_MVLS.png", "/assets/img/MVLS-videollamada.png", "/assets/img/MVLS-Videollamada2.png"];
+const ImagenesConsultancy = []
+const btn_izquierda = document.querySelector(".btn-izquierda");
+const btn_derecha = document.querySelector(".btn-derecha");
+const contenido_imgen = document.querySelector(".contenido-modal");
+
+let index = 0;
+
+function Recorrer_Imagenes(imagenes, contenido){
+
+    if(contenido){
+        contenido.style.background = `url(${imagenes[index]})`;
+    }
+
+}
+
+function Cambiar_imagen(button_derecho, button_izquierdo, imagenes){
+
+    if(button_derecho){
+
+        button_derecho.addEventListener("click", (e) => {
+            index ++;
+
+            if(index >= imagenes.length){
+                index = 0;
+            }
+
+             Recorrer_Imagenes(ImagenesDataMVLS, contenido_imgen);
+        })
+    }
+
+    if(button_izquierdo){
+
+        button_izquierdo.addEventListener("click", (e) =>{
+           index --;
+
+           if(index < 0){
+
+            index  = imagenes.length - 1;
+           }
+
+            Recorrer_Imagenes(ImagenesDataMVLS, contenido_imgen);
+        })
+    }
+}
+
+Cambiar_imagen(btn_derecha, btn_izquierda, ImagenesDataMVLS);
+
