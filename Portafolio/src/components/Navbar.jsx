5bc5364ac9832d3f3af8d1e928645c8108950485
yUMIP04 <yumip5719@gmail.com>
2026-08-09 21:53:12 -0500

    -Colocandole el estilo al navbar.

--- Portafolio/src/components/Navbar.jsx
+++ Portafolio/src/components/Navbar.jsx
@@ -1,13 +1,15 @@
+import '../Navbar.css'
+
 
 export default function Button(){
 
     return(
         
-       <header class="nav">
-        <ul class="navigation">
-            <li class="item active" id="Sobre-Mi"><a href="#aboutME">Sobre Mi</a></li>
-            <li class="item" id="Proyectos"><a href="#proyectos">Proyectos</a></li>
-            <li class="item" id="Contacto"><a href="#contacto">Contacto</a></li>
+       <header className="nav">
+        <ul className="navigation">
+            <li className="item active" id="Sobre-Mi"><a href="#aboutME">Sobre Mi</a></li>
+            <li className="item" id="Proyectos"><a href="#proyectos">Proyectos</a></li>
+            <li className="item" id="Contacto"><a href="#contacto">Contacto</a></li>
         </ul>
     </header>
     );
