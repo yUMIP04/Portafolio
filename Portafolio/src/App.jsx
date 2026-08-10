7a925fd72e196cee8c6eb6babdf4f1e8f56992e2
yUMIP04 <yumip5719@gmail.com>
2026-08-09 21:08:28 -0500

    -Actualizando portafolio y rehaciendolo con React.

---
+++ Portafolio/src/App.jsx
@@ -0,0 +1,22 @@
+/*import { useState } from 'react'
+*/
+
+import Button from './components/Navbar.jsx'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    
+     
+       <Button />
+    </>
+
+ 
+  )
+}
+
+export default App
---
+++ Portafolio/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+
+export default function Button(){
+
+    return(
+        
+       <header class="nav">
+        <ul class="navigation">
+            <li class="item active" id="Sobre-Mi"><a href="#aboutME">Sobre Mi</a></li>
+            <li class="item" id="Proyectos"><a href="#proyectos">Proyectos</a></li>
+            <li class="item" id="Contacto"><a href="#contacto">Contacto</a></li>
+        </ul>
+    </header>
+    );
+}
