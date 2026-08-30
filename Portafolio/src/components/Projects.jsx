3d2ebeaa283f8714c62974e61ca16a2a125bdd8c
yUMIP04 <yumip5719@gmail.com>
2026-08-29 19:49:45 -0600

    -Empezando el apartado de Proyectos.-Empezando la primera card que tiene el primero proyecto.

--- Portafolio/src/components/Projects.jsx
+++ Portafolio/src/components/Projects.jsx
@@ -19,6 +19,69 @@ export default function Projects(){
             
             <div className='Proyect1'> 
                 <div className="titulo-Proyect" style={{display:'flex', flexDirection:'column', justifyContent:'center'}}>
+                
+                <h1 style={{color:'white', textAlign:'center'}}>MVLS</h1>
+                <div style={{display:'flex', flexDirection:'row', justifyContent:'center'}}>
+                <hr className='Barra-Titulo' />
+                </div>
+
+            </div>
+
+            <div className="info-project">
+            
+            <div className="img">
+            <div className="IMG-Project"></div>
+</div>
+
+<div className="descrip-project">
+    <p style={{color: 'white', textAlign:'center', marginTop:'8px'}} className='parrafo-project'>MVLS es una aplicacion que brinda soporte y mantenimiento en equipo de computo 
+por medio de una videollamada y por un chat en vivo, se debe ingresar con el correo
+ y contraseña que registraste en la pagina</p>
+</div>
+
+<div className="use-skills">
+    <div className="skill">
+        <p className='blur-sm'>electron</p>
+    </div>
+
+    <div className="skill">
+        <p>HTML</p>
+    </div>
+
+    <div className="skill">
+        <p>CSS</p>
+    </div>
+
+    <div className="skill">
+        <p>JavaScript</p>
+    </div>
+
+    <div className="skill">
+        <p>express.js</p>
+    </div>
+
+    <div className="skill">
+        <p>agora</p>
+    </div>
+
+    <div className="skill">
+        <p>webrtc</p>
+    </div>
+
+    <div className="skill">
+        <p>mysql</p>
+    </div>
+
+</div>
+
+            </div>
+
+</div>
+
+{/*🌟Proyect 2 */}
+
+    <div className='Proyect1'> 
+                <div className="titulo-Proyect" style={{display:'flex', flexDirection:'column', justifyContent:'center'}}>
                 <h1 style={{color:'white', textAlign:'center'}}>MVLS</h1>
                 <div style={{display:'flex', flexDirection:'row', justifyContent:'center'}}>
                 <hr className='Barra-Titulo' />
